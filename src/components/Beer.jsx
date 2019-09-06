879522aa22b1f4b3a903603fce54146ee8cb928b
Alexander Sergejev <alexandersergejev@gmail.com>
2019-09-06 16:46:52 -0700

    attempt to add more dynamic functionality

--- src/components/Beer.jsx
+++ src/components/Beer.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 
+var beer = {
+  name: "Beer",
+  brand: "Beer is Us",
+  price: "3.00",
+  abv: ".03",
+  pints: "124"
+}
 
 const Beer = () => (
     <div>
       <h1> About this keg:</h1>
       <ul>
-        <li>Name: Beer</li>
-        <li>Brand: Beer is Us</li>
-        <li>Price: $3.00 per pint</li>
-        <li>Alcohol by volume: 3% </li>
-        <li>Pints left in keg: 124 </li>
+        <li>Name: {beer.name}</li>
+        <li>Brand: {beer.brand} </li>
+        <li>Price: ${beer.price} per pint</li>
+        <li>Alcohol by volume: {beer.abv * 100}% </li>
+        <li>Pints left in keg: {beer.pints}</li>
       </ul>
     </div>
   );
--- src/components/BrothersIPA.jsx
+++ src/components/BrothersIPA.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 
+ var bipa = {
+   name: "Brother's IPA",
+   brand: "Bro's Brewing",
+   price: "5.00",
+   abv: ".06",
+   pints: "124"
+ }
 
 const BrothersIPA = () => (
     <div>
       <h1> About this keg:</h1>
       <ul>
-        <li>Name: Brother's IPA</li>
-        <li>Brand:Bro's Brewing</li>
-        <li>Price: $5.00 per pint</li>
-        <li>Alcohol by volume: 6%</li>
-        <li>Pints left in keg: 124</li>
+        <li>Name: {bipa.name}</li>
+        <li>Brand: {bipa.brand} </li>
+        <li>Price: ${bipa.price} per pint</li>
+        <li>Alcohol by volume: {bipa.abv * 100}% </li>
+        <li>Pints left in keg: {bipa.pints}</li>
       </ul>
     </div>
   );
