657289e1a12500260c2b1473de93b9026bc29b4c
Alexander Sergejev <alexandersergejev@gmail.com>
2019-09-06 16:50:12 -0700

    continue changes

--- src/components/ThisPorter.jsx
+++ src/components/ThisPorter.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 
+var porter = {
+  name: "This Porter",
+  brand: "That Brewery",
+  price: "6.00",
+  abv: ".04",
+  pints: "124"
+}
+
 const ThisPorter = () => (
     <div>
       <h1> About this keg:</h1>
       <ul>
-        <li>Name: This Porter</li>
-        <li>Brand: That Brewery</li>
-        <li>Price: $6.00 per pint</li>
-        <li>Alcohol by volume:  4% </li>
-        <li>Pints left in keg: 124</li>
+      <li>Name: {porter.name}</li>
+      <li>Brand: {porter.brand} </li>
+      <li>Price: ${porter.price} per pint</li>
+      <li>Alcohol by volume: {porter.abv * 100}% </li>
+      <li>Pints left in keg: {porter.pints}</li>
       </ul>
     </div>
   );
