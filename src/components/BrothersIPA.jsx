0b333762eccd227d9c5ea790c6f64e9ea121686e
Student <example@example.com>
2019-09-06 13:25:19 -0700

    continue fleshing out site

--- src/components/BrothersIPA.jsx
+++ src/components/BrothersIPA.jsx
@@ -1,1 +1,18 @@
 import React from 'react';
+
+
+const BrothersIPA = () => (
+    <div>
+      <h1> About this keg:</h1>
+      <ul>
+        <li>Name:</li>
+        <li>Brand:</li>
+        <li>Price: </li>
+        <li>Alcohol by volume:</li>
+        <li>Pints left in keg: </li>
+      </ul>
+    </div>
+  );
+
+
+export default BrothersIPA;
