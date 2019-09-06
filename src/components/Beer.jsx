73293221767c43387a5e1ec34637be665eb641ea
Student <example@example.com>
2019-09-06 12:05:16 -0700

    Fix route for individual keg

--- src/components/Beer.jsx
+++ src/components/Beer.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 const Beer = () => (
     <div>
-    <h1> About this keg:</h1>
-    <ul>
-      <li>Name:</li>
-      <li>Brand:</li>
-      <li>Price: </li>
-      <li>Alcohol by volume</li>
-      <li>Pints left in keg: </li>
-    </ul>
+      <h1> About this keg:</h1>
+      <ul>
+        <li>Name:</li>
+        <li>Brand:</li>
+        <li>Price: </li>
+        <li>Alcohol by volume:</li>
+        <li>Pints left in keg: </li>
+      </ul>
     </div>
-  )
+  );
 
 
-export default Beer
+export default Beer;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export { default as Home } from './Home';
-export { default as AllKegs } from './AllKegs';
+export { default as Beer } from './Beer';
 export { default as NewKegForm } from './NewKegForm';
 export { default as Error404 } from './Error404';
