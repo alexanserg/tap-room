5941dbee3dc3584bcae5a02034ce00e087d2277d
Alexander Sergejev <alexandersergejev@gmail.com>
2019-09-13 16:44:29 -0700

    continue refactoring

---
+++ src/components/Keg.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Keg(props) {
+  const kegInformation =
+    <div>
+      <style jsx>{'div {background-color: red;}'}</style>
+      <h2>{props.brand} - {props.name}</h2>
+      <hr/>
+    </div>;
+  if (props.currentRouterPath === '/kegController') {
+    return (
+      <div onClick={() => {props.onKegSelection(props.kegId);}}>
+        {kegInformation}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {kegInformation}
+      </div>
+    );
+  }
+}
+
+
+Keg.propTypes = {
+  name: PropTypes.string.isRequired,
+  brand: PropTypes.string.isRequired,
+  issue: PropTypes.string,
+  formattedWaitTime: PropTypes.string.isRequired,
+  currentRouterPath: PropTypes.string,
+  onKegSelection: PropTypes.func,
+  kegId: PropTypes.string.isRequired
+};
+
+export default Keg;
---
+++ src/components/KegList.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Keg from './Keg';
+import PropTypes from 'prop-types';
+
+
+
+function KegList(props) {
+  return(
+    <div>
+      <hr/>
+      {Object.keys(props.kegList).map(function(kegId) {
+        var keg = props.kegList[kegId];
+        return <Keg names={keg.names}
+        location={keg.brand}
+        issue={keg.abv}
+        price={keg.price}
+        currentRouterPath={props.currentRouterPath}
+        key={kegId}
+        kegId={kegId}
+        onKegSelection={props.onKegSelection} />
+      })}
+    </div>
+  );
+}
+
+
+KegList.propTypes = {
+  kegList: PropTypes.object,
+  currentRouterPath: PropTypes.string,
+  onKegSelection: PropTypes.func
+};
+
+export default KegList;
