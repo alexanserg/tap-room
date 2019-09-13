3890610de84d13a6ba70733ca990adf8a3f54f45
Student <example@example.com>
2019-09-13 15:45:18 -0700

    continue to refactor

---
+++ src/components/NewKegControl.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import ConfirmationQuestions from './ConfirmationQuestions';
+import NewKegForm from './NewKegForm';
+import PropTypes from 'prop-types';
+
+class NewKegControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+    this.handleTroubleShootingConfirmation = this.handleTroubleShootingConfirmation.bind(this);
+  }
+
+  handleTroubleShootingConfirmation(){
+    this.setState({formVisibleOnPage: true});
+    document.body.style.backgroundColor = '#FFC926';
+
+  }
+
+  render(){
+    let currentlyVisibleContent = null;
+    if (this.state.formVisibleOnPage){
+      currentlyVisibleContent = <NewKegForm onNewKegCreation={this.props.onNewKegCreation}/>;
+    } else {
+      currentlyVisibleContent = <ConfirmationQuestions onTroubleShootingConfirmation={this.handleTroubleShootingConfirmation}/>;
+    }
+    return (
+      <div>
+        {currentlyVisibleContent}
+      </div>
+    );
+  }
+}
+
+NewKegControl.propTypes = {
+  onNewKegCreation: PropTypes.func
+};
+
+export default NewKegControl;
---
+++ src/components/NewKegForm.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function NewKegForm(props){
+  let _names = null;
+  let _brand = null;
+  let _abv = null;
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    props.onNewKegCreation({names: _names.value, brand: _brand.value, abv: _abv.value, price: _price.value);
+    _names.value = '';
+    _brand.value = '';
+    _abv.value = '';
+  }
+  return (
+    <div>
+      <form onSubmit={handleNewKegFormSubmission}>
+        <input
+          type='text'
+          id='names'
+          placeholder='Pair Names'
+          ref={(input) => {_names = input;}}/>
+        <input
+          type='text'
+          id='brand'
+          placeholder='Location'
+          ref={(input) => {_brand = input;}}/>
+        <input
+          type="number"
+          id='abv'
+          placeholder='What is the alcohol content?'
+          ref={(input) => {_abv = input;}}/>
+        <input
+          type='number'
+          id='price'
+          placeholder='What is the price per pint?'
+          ref={(input) => {_price = input;}}/>
+        <button type='submit'>Help!</button>
+      </form>
+    </div>
+  );
+}
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+};
+
+export default NewKegForm;
